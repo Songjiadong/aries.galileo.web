65b9b35c5a8bc58134e7897fcbf8117567ad16c1
xn <xuening@miic.com.cn>
2024-08-16 11:25:33 +0800

    初测vue3版本,未完成

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<script setup>
+import SearchInput from './common/SearchInput.vue'
+import ListTitle from './common/ListTitle.vue'
+
+</script>
+
+<template>
+    <main>
+        <div class="main-wrapper">
+            <!-- 产品信息 -->
+            <div class="produce-info">
+                <div class="produce-logo">
+                    <img src="../assets/images/logo.svg" alt="LOGO">
+                </div>
+                <div class="produce-text">
+                    <div class="produce-name">机械行业AI搜索</div>
+                    <div class="produce-version">v0.1</div>
+                </div>
+            </div>
+            <!-- 主搜索 -->
+            <SearchInput />
+            <!-- 参考提问 -->
+            <ListTitle />
+        </div>
+        <div class="footer-wrapper">
+            <div><router-link to="/aboutUs">关于我们</router-link></div>
+            <div><router-link to="/contactUs">联系我们</router-link></div>
+            <div>MIIC©2024</div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+@import url(../assets/css/home.scss);
+</style>
---
+++ src/components/common/ListTitle.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="produce-titlelist">
+        <ul>
+            <li>中国机械工业联合会</li>
+            <li>机械工业发展论坛什么时候举办？</li>
+        </ul>
+    </div>
+</template>
